e0836b2c7f22ebc4cd10df405c6f24496ed2e11f
anurag-babal <babalanurag2@gmail.com>
2024-01-31 02:54:48 +0530

    Update database configuration

--- src/main/java/com/example/academia/student/StudentController.java
+++ src/main/java/com/example/academia/student/StudentController.java
@@ -23,33 +23,34 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class StudentController {
-    private final StudentMapper studentMapper;
-    private final StudentService studentService;
+  private final StudentMapper studentMapper;
+  private final StudentService studentService;
 
-    @GetMapping("/v1/students")
-    public ResponseDto getAllStudents(Pageable pageable) {
-        List<Student> students = studentService.getAllStudents(pageable).getContent();
-        return ResponseBuilder.createListResponse(students.size(), students);
-    }
+  @GetMapping("/v1/students")
+  public ResponseDto getAllStudents(Pageable pageable) {
+    List<Student> students = studentService.getAllStudents(pageable).getContent();
+    return ResponseBuilder.createListResponse(students.size(), students);
+  }
 
-    @GetMapping("/v1/students/{id}")
-    public ResponseDto getStudentById(@PathVariable(name = "id") int id) {
-        Student student = studentService.getStudentById(id);
-        return ResponseBuilder.createResponse(student);
-    }
+  @GetMapping("/v1/students/{id}")
+  public ResponseDto getStudentById(@PathVariable(name = "id") int id) {
+    Student student = studentService.getStudentById(id);
+    return ResponseBuilder.createResponse(student);
+  }
 
-    @GetMapping("/v1/students/courses/{courseId}")
-    public ResponseDto getStudentsForCourseId(
-            @PathVariable(name = "courseId") int courseId,
-            Pageable pageable) {
-        List<Student> students = studentService.getAllStudentsForCourseId(courseId, pageable).getContent();
-        return ResponseBuilder.createListResponse(students.size(), students);
-    }
+  @GetMapping("/v1/students/courses/{courseId}")
+  public ResponseDto getStudentsForCourseId(@PathVariable(name = "courseId") int courseId,
+      Pageable pageable) {
+    List<Student> students =
+        studentService.getAllStudentsForCourseId(courseId, pageable).getContent();
+    return ResponseBuilder.createListResponse(students.size(), students);
+  }
 
-    @PostMapping("/v1/students")
-    public ResponseDto createStudentAdmission(@Valid @RequestBody StudentAdmissionCreateRequestDto dto) {
-        Student student = studentMapper.mapToDomainStudent(dto);
-        Student createdStudent = studentService.createStudentAdmission(student);
-        return ResponseBuilder.createResponse(createdStudent);
-    }
+  @PostMapping("/v1/students")
+  public ResponseDto createStudentAdmission(
+      @Valid @RequestBody StudentAdmissionCreateRequestDto dto) {
+    Student student = studentMapper.mapToDomainStudent(dto);
+    Student createdStudent = studentService.createStudentAdmission(student);
+    return ResponseBuilder.createResponse(createdStudent);
+  }
 }
